6b8ee910e2a21a14752786fd1eb19d9966d1e6bc
Primož Jurič <104359153+PrimozJu@users.noreply.github.com>
2024-06-04 15:35:45 +0200

    Enable Cors on functions

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,12 @@ const logger = require("firebase-functions/logger");
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
-app.use(express.json());
+const cors = require('cors');
 
-const crud = require("./crud");
+app.use(express.json());
+app.use(cors()); //Zacasno omogocimo CORS v lokalnem okolju
+
+const crud = require("./crud"); 
 app.use("/", crud);
 
 exports.api = functions.https.onRequest(app);
